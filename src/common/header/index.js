af91e05cea05373042af0d2de796e73fc89ba5cc
jiangdonghan <2641635339@qq.com>
2020-04-27 00:08:42 +0800

    use stateless header component

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -12,40 +12,38 @@ import {
   SearchWrapper
 } from './style'
 //任何组件开头都要大写
-class Header extends Component {
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载App</NavItem>
-          <NavItem className='right'>登录</NavItem>
-          <NavItem className='right'>
-            <i className="iconfont">&#xe602;</i>
-          </NavItem>
-          <SearchWrapper>
-            <CSSTransition 
-            in={this.props.focused}
-            timeout={200}
-            classNames = "slide"            
-            >
-              <NavSearch
-                className = {this.props.focused ? 'focused':''}
-                onFocus = {this.props.hanldeInputFocus}
-                onBlur = {this.props.hanldeInputBlur}
-              ></NavSearch>
-              </CSSTransition>
-              <i className={this.props.focused ? 'focused iconfont':'iconfont'}>&#xe622;</i>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className="writing"><i className="iconfont">&#xe70d;</i>写文章</Button>
-          <Button className="reg">注册</Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
+const Header = (props)=>{
+  return(
+    <HeaderWrapper>
+    <Logo />
+    <Nav>
+      <NavItem className='left active'>首页</NavItem>
+      <NavItem className='left'>下载App</NavItem>
+      <NavItem className='right'>登录</NavItem>
+      <NavItem className='right'>
+        <i className="iconfont">&#xe602;</i>
+      </NavItem>
+      <SearchWrapper>
+        <CSSTransition 
+        in={props.focused}
+        timeout={200}
+        classNames = "slide"            
+        >
+          <NavSearch
+            className = {props.focused ? 'focused':''}
+            onFocus = {props.hanldeInputFocus}
+            onBlur = {props.hanldeInputBlur}
+          ></NavSearch>
+          </CSSTransition>
+          <i className={props.focused ? 'focused iconfont':'iconfont'}>&#xe622;</i>
+      </SearchWrapper>
+    </Nav>
+    <Addition>
+      <Button className="writing"><i className="iconfont">&#xe70d;</i>写文章</Button>
+      <Button className="reg">注册</Button>
+    </Addition>
+  </HeaderWrapper>
+  )
 }
 
 const mapStateToProps = (state) => {
